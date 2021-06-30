626b2c907cf74af5244b514bf56d4d36d1223106
Doderovic Milan <mdoderovic5@gmail.com>
2021-06-30 11:03:19 +0200

    QR code and Bar code generator

---
+++ AplicationQR/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using AplicationQR.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicationQR.Controllers
+{
+    [Route("demo")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("generate")]
+        public IActionResult Generate(string qrtext)
+        {
+            ViewBag.qrtext = qrtext;
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
